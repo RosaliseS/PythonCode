80e219bf7085a8a69b646bf8a445fdda89845613
RosaliseS <rosalise@outlook.com>
2026-03-08 12:30:57 +0000

    Add files via upload

---
+++ storing_birthdays.py
@@ -0,0 +1,10 @@
+sophia = "5th March"
+emma = "12th April"
+olivia = "20th May"
+rebecca = "15th June"
+sylvie = "10th July"
+print("Sophia's birthday is on", sophia)
+print("Emma's birthday is on", emma)
+print("Olivia's birthday is on", olivia)
+print("Rebecca's birthday is on", rebecca)
+print("Sylvie's birthday is on", sylvie)
