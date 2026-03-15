2c9f6b76c914f62d463ebabecd021c0afb45b273
Masum Shaikh <masum.shaikh@finbourne.com>
2026-03-15 12:12:22 +0000

    Clean up file names

--- Rosa.py
+++
@@ -1,9 +0,0 @@
-print("hello world!" \
-"")
-print(5)
-print("it is 11:09a.m.")
-print("Welcome to the world of programming!")
-print(55)
-print("I am learning Python.\n")
-print("today it is March 8,2026")
-print("welcome to",end="*")
